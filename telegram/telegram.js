dcfee6d3a52d01bc3df485804deb94f16742bb17
intfox <initfox@gmail.com>
2020-05-11 22:06:52 +0300

    not work?wtf?

--- telegram/telegram.js
+++ telegram/telegram.js
@@ -1,4 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api')
+const { LogicError } = require('../model/error')
 
 module.exports = class Telegram {
     constructor(config, dialogsService, translateService, userSettingService) {
@@ -17,36 +18,36 @@ module.exports = class Telegram {
 
         this.bot.onText(/^\/create$/, (msg) => {
             const chatId = msg.chat.id
-            dialogsService.create({id: chatId, username: msg.from.username}).then(dialogId => {
+            _errorHandling(chatId, dialogsService.create({id: chatId, username: msg.from.username}).then(dialogId => {
                 this.bot.sendMessage(chatId, `bot: that dialog id:`)
                 this.bot.sendMessage(chatId, dialogId)
-            })
+            }))
         })
 
         this.bot.onText(/^\/join ([A-z, 0-9]{8})$/, (msg, arr) => {
             const dialogId = arr[1]
-            dialogsService.join({id: msg.chat.id, username: msg.from.username}, dialogId).then(arrUsers => {
+            _errorHandling(msg.chat.id, dialogsService.join({id: msg.chat.id, username: msg.from.username}, dialogId).then(arrUsers => {
                 console.log(`arrUsers: ${JSON.stringify(arrUsers)}`)
                 arrUsers.forEach(user => {
                     this.bot.sendMessage(user.id, `bot: @${msg.from.username} joined`)
                 })
                 this.bot.sendMessage(msg.chat.id, 'bot: joined successed')
-            })
+            }))
         })
 
         this.bot.onText(/^\/setLanguage (.*)$/, (msg, arr) => {
             const language = arr[1]
-            translateService.supportedLanguage().then( supportedLanguages => {
+            _errorHandling(msg.chat.id, translateService.supportedLanguage().then( supportedLanguages => {
                 if(supportedLanguages.includes(language)) {
                     userSettingService.setLanguage(msg.chat.id, language)
                 } else {
                     this.bot.sendMessage(msg.chat.id, 'bot: this language not supported')
                 }
-            })
+            }))
         })
 
         this.bot.onText(/^[^\/]./, msg => {
-            dialogsService.getDialogFromUserId(msg.chat.id).then(dialog =>
+            _errorHandling(msg.chat.id, dialogsService.getDialogFromUserId(msg.chat.id).then(dialog =>
                 userSettingService.getLanguage(msg.chat.id).then(languageSender => {
                     dialog.users.forEach(user => {
                         if(user.id != msg.chat.id) userSettingService.getLanguage(user.id).then(language => {
@@ -56,7 +57,17 @@ module.exports = class Telegram {
                         })
                     })
                 })
-            )
+            ))
+        })
+    }
+
+    _errorHandling(chatId, promise) {
+        return promise.catch(err => {
+            if(err instanceof LogicError) {
+                this.bot.sendMessage(chatId, `bot: ${err.message}`)
+            } else {
+                console.error(`err: ${err.name} ${err.message}`)
+            }
         })
     }
 }
